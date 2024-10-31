cd153c929e2187c32adcd03489d48b6e0e8286e1
erandie <hansierandi7@gmail.com>
2024-11-01 04:26:42 +0530

    pos_system done again-02

---
+++ controller/LoginController.js
@@ -0,0 +1,13 @@
+$("#login").on('click', function () {
+
+    const email = "hansierandi7@gmail.com";
+    const pw = "hans72";
+
+    let inputEmail = $("#inputEmail3").val();
+    let inputPW = $("#inputPassword3").val();
+
+
+
+
+
+})
